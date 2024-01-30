463229a12069b9ff72d0e25908c69f1c6cdf9374
Ikey Guterres <iikeyguterres@gmail.com>
2024-01-30 13:41:01 +0000

    Criação de novos cards usando props

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,18 @@ function App() {
     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
   const item2= {
-    name: 'Mother Smith',
-    image: ''
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
   return (
     <>
       <Card item={item1} />
-      <Card item={item1} />
+      <Card item={item2} />
+      <Card item={item3} />
     </>
   )
 }
