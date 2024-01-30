b62df1a804fe107434848a49477689eb2e5c98b9
Ikey Guterres <iikeyguterres@gmail.com>
2024-01-30 13:36:47 +0000

    iniciando configurações props

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 ={
+    name:'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+  const item2= {
+    name: 'Mother Smith',
+    image: ''
+  }
   return (
     <>
-      <Card />
-      <Card />
+      <Card item={item1} />
+      <Card item={item1} />
     </>
   )
 }
