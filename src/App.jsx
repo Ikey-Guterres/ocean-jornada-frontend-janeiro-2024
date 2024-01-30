2b7f3a9b2ce652b516be7631fd0269361e4979f9
Ikey Guterres <iikeyguterres@gmail.com>
2024-01-30 14:38:23 +0000

    Exibindo implementação do map para visualizar cada item de uma lista

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const item1 ={
-    name:'Rick Sanchez',
+  const item1 = {
+    name: 'Rick Sanchez',
     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
-  const item2= {
+  const item2 = {
     name: 'Morty Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
   }
@@ -15,11 +15,19 @@ function App() {
     name: 'Summer Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
+
+  const itens =[item1, item2, item3, item1]
+
   return (
     <>
-      <Card item={item1} />
-      <Card item={item2} />
-      <Card item={item3} />
+      <div className="cards">
+        {/* <Card item={item1} />
+        <Card item={item2} />
+        <Card item={item3} /> */}
+        {
+          itens.map(item => <Card item = {item}/>)
+        }
+      </div>
     </>
   )
 }
