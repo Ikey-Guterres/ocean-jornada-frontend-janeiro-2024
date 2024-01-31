ba06c80ce87def143388dc991409b12f53471f1d
Ikey Guterres <iikeyguterres@gmail.com>
2024-01-31 13:32:44 +0000

    Carregamento de dados via API

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  //Carregamento de dados direto via código
   const item1 = {
     name: 'Rick Sanchez',
     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
@@ -19,13 +20,35 @@ function App() {
     name: 'Beth Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
   }
-  const itens =[item1, item2, item3, item4]
+  const itens = [item1, item2, item3, item4]
 
+  async function carregarDadosAPI() {
+    // Carregamento de dados via API, usando npm fetch
+    const apiURL = 'https://rickandmortyapi.com/api/character/'
+
+    const response = await fetch(apiURL)
+    
+    //console.log(response)
+
+    const body = await response.json()
+
+    console.log(body)
+
+    //Extrair a propriedade results do body
+    //Essa propriedade contém a lista de itens
+    const results = body.results;
+
+    /*TODO: Enviar os 'results' para o JSX, exibindo
+    um card para cada item da API e depois subir o
+    site na nuvem */
+  }
+
+  carregarDadosAPI()
   return (
     <>
       <div className="cards">
         {
-          itens.map((item, i) => <Card item = {item} key={i}/>)
+          itens.map((item, i) => <Card item={item} key={i} />)
         }
       </div>
     </>
